df1b6bec4a278bb9a967a54151a1f755575161af
sharwenn <magnifico731@gmail.com>
2026-09-20 20:40:15 -0500

    9/20 animation things
    - added new idle and walk animations
    - new aim reticle thing and made head and reticle use the same rotation
    - made walking animation reverse if moving backwards
    - new player return function is_moving_backwards()
    - new animation_controller functions set_animation_speed(anim_speed) and set_animation_direction()
    - mouse position now updated in physics_process in player script and a starting variable in animation_controller

--- player.gd
+++ player.gd
@@ -4,7 +4,10 @@ signal defeat
 @export var stats: PlayerStats
 @onready var state_machine: StateMachine = $StateMachine
 @onready var animation_controller: PlayerAnimationController = $AnimationController
+@onready var mouse_pos = get_global_mouse_position()
+
 var dead = false
+
 func _ready() -> void:
 	if not stats:
 		stats = PlayerStats.new()
@@ -17,7 +20,16 @@ func take_damage(damage):
 		stats.health = 0
 		defeat.emit(name,Time.get_ticks_msec()) #emit(nameofplayer,timeofdeath)
 		
+
+func is_moving_backwards() -> bool:
+	if velocity.x == 0.0:
+		return false
 	
+	var moving_dir = sign(velocity.x)
+	var mouse_dir = sign(mouse_pos.x - global_position.x)
+	
+	return moving_dir != mouse_dir
+
 func _process(delta: float) -> void:
 	state_machine.update(delta)
 func _input(event: InputEvent) -> void:
@@ -27,6 +39,8 @@ func _input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	state_machine.physics_update(delta)
 	var direction = Input.get_axis("left", "right")
+	mouse_pos = get_global_mouse_position()
+	
 	velocity.x = direction * stats.base_move_speed
 
 	move_and_slide()
--- scripts/player/player_animation_controller.gd
+++ scripts/player/player_animation_controller.gd
@@ -1,31 +1,44 @@
 class_name PlayerAnimationController
 extends Node
 
+@onready var mouse_pos = owner.get_global_mouse_position()
+
 @export var animation_player: AnimationPlayer
 @onready var body_sprite = owner.find_child("Body")
 @onready var head_sprite = owner.find_child("Head")
 @onready var reticle = owner.find_child("AimHandler")
 
-@export var head_offset = 15
+@export var head_offset = 30
+@export var base_anim_speed = 1.0
+@export var current_anim_speed = base_anim_speed
 
 func play_animation(anim_name: String) -> void:
 	if animation_player and animation_player.current_animation != anim_name:
 		animation_player.play(anim_name)
 
+func set_animation_speed(anim_speed: float) -> void:
+	animation_player.speed_scale = anim_speed
+
+func set_animation_direction() -> void:
+	if owner.is_moving_backwards(): # in the future this should only apply to certain states
+		set_animation_speed(-current_anim_speed)
+	else:
+		set_animation_speed(current_anim_speed)
+
 func set_facing_direction() -> void:
-	if owner.has_node("Body"):
-	#	var direction := Input.get_axis("left", "right")
+	if owner.has_node("Body"): # in the future we should be able to choose between the two versions below per state
+	#	var direction := Input.get_axis("left", "right")  
 	#	if direction != 0:
 	#		body_sprite.flip_h = direction < 0
-		var mouse_pos = owner.get_global_mouse_position()
+		mouse_pos = owner.get_global_mouse_position()
 		if mouse_pos.x > owner.position.x:
 			body_sprite.flip_h = false
 		elif mouse_pos.x < owner.position.x:
 			body_sprite.flip_h = true
 
+# will refactor later for efficiency 
 func set_head_rotation() -> void:
-	var mouse_pos = owner.get_global_mouse_position()
-	head_sprite.look_at(mouse_pos)
+	head_sprite.rotation = reticle.rotation
 	if mouse_pos < head_sprite.get_global_position():
 		head_sprite.flip_v = true
 		head_sprite.offset.y = -head_offset
@@ -34,12 +47,12 @@ func set_head_rotation() -> void:
 		head_sprite.offset.y = head_offset
 
 func rotate_reticle() -> void:
-	var mouse_pos = owner.get_global_mouse_position()
-	
 	reticle.look_at(mouse_pos)
 
-func _process(delta: float) -> void:
+
+func _process(_delta: float) -> void:
 	if owner:
 		set_facing_direction()
+		set_animation_direction()
 		set_head_rotation()
 		rotate_reticle()
